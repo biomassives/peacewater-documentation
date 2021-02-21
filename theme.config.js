4ca003ab2194d7ce0508303b21c9d7d5cb3319e9
G. Willson <greg@biomassiv.es>
2021-02-20 18:45:42 -0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,11 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/biomassives/peacewater-documentation',
+  titleSuffix: ' – Peacewater',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next Site Builder
+        Peacewater Documentation
       </span>
     </>
   ),
@@ -15,14 +15,14 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the next site builder" />
-      <meta name="og:description" content="Nextra: the next site builder" />
+      <meta name="description" content="Peacewater Database system for water technology solutions" />
+      <meta name="og:description" content="Peacewater documentation" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="twitter:image" content="https://peacewater-documentation.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="og:title" content="Peacewater Database Documentation" />
+      <meta name="og:image" content="https://peacewater-documentation.vercel.app/og.png" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
       <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
